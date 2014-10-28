11a92849a60832335edaec12f3754ff54c5a2f50
FMCalisto <francisco_mcalisto@hotmail.com>
2014-10-28 12:01:34 +0000

    Update

--- src/positives-array.lisp
+++ src/positives-array.lisp
@@ -1,11 +1,9 @@
 ;;;; Positive Numbers of Array ;;;;
 
 
-(defun matrix-search (matrix &aux res)
+(defun positives-array (matrix &aux res)
         (dotimes (lin (array-dimension matrix 0))
                 (dotimes (col (array-dimension matrix 1))
                         (when (> (aref matrix lin col) 3)
                                 (setf res (cons (aref matrix lin col) res)))))
         res)
-        
-(print (matrix-search array))
