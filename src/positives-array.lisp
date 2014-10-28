faebd8fb937d6307114e8d129c206061bb63b6d8
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2014-10-28 11:07:22 +0000

    Update
    New update file.

--- src/positives-array.lisp
+++ src/positives-array.lisp
@@ -3,11 +3,11 @@
 ; Note: this is not well.
 
 (load "join.lisp")
+(load "any-find-if.lisp")
 
 (defun positives-array (l1 l2)
-	#'(lambda (nl)
-			(if ((first l1) >= 0)
-					(join nl (first l1))
-				(if ((first l2) >= 0)
-						(join nl (first l2))
-					(positives-array (rest l1) (rest 2))))))
+   (if (any-find-if (< p 0) (first l1))
+          (join (rest l1) l2))
+      (if (any-find-if (< p 0) (first l2))
+        		(join l1 (rest l2)))
+        (T NIL)))
