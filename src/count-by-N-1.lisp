7f054c8f84c8c668c05b8ce0d0bb473506e2c679
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-07-07 19:04:20 +0100

    Update count-by-N-1.lisp

--- src/count-by-N-1.lisp
+++ src/count-by-N-1.lisp
@@ -1,5 +1,5 @@
 ;;;; Finding the difference in an arithmetic progression ;;;;
-[@StackOverflow](http://stackoverflow.com/questions/31221824/finding-the-difference-in-an-arithmetic-progression-in-lisp/31275954#31275954)
+;;;; http://stackoverflow.com/questions/31221824/finding-the-difference-in-an-arithmetic-progression-in-lisp/ ;;;;
 
 (defun count-by-N-1 (lst)
   (if (equal NIL lst)
