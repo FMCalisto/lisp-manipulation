a9d0b6989fcfcd63e8652ed1ec42bb0d3c7afc03
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-07-07 20:12:39 +0100

    Update sum-a2.lisp

--- src/sum-a2.lisp
+++ src/sum-a2.lisp
@@ -1,7 +1,7 @@
 ;;;; Sums tow Arrays and returns the sum of the both ;;;;
 
 (defun sum-a2 (array1 array2)
-  (if (or (null array1 (null array2))
+  (if (or (null array1) (null array2))
     NIL
     (cons
       (+ (first array1) (first array2))
