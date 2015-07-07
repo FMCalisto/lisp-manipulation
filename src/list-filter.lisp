ac08f629ae36d542d0e8d88de5969d9d6e076952
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-07-07 20:31:53 +0100

    Create list-filter.lisp

---
+++ src/list-filter.lisp
@@ -0,0 +1,14 @@
+;;;; A function filter lists that takes as arguments an integer list and a predicate and
+;;;; returns a new list containing the elements of the original list checking the predicate.
+
+;;;; For example (note that mod returns the remainder of the integer division)
+;;;; > (list-filter '(1 2 3 4 5 6) #' (lambda (x) (zerop (3 x mod))))
+;;;; (3 6)
+
+(defun list-filter (lst pred)
+  (if (null lst)
+    NIL
+    (funcall op (first lst))
+  )
+  (list-filter (rest lst) pred)
+)
