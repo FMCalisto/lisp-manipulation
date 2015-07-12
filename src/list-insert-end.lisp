29a94ef0933d09dc9bf08fa6866c5b34a6adc514
Francisco Maria Calisto <francisco_mcalisto@hotmail.com>
2015-07-13 00:02:09 +0100

    [ADD] List Insert End
    Adding new file to the source.

---
+++ src/list-insert-end.lisp
@@ -0,0 +1,13 @@
+;;;; Recursively the function inserts end receiving a x element into a list
+;;;; and inserts the element at the end of the list.
+;;;; > (list-insert-end 5 ‘(1 2 4 6))
+;;;; (1 2 4 6 5)
+;;;; > (list-insert-end 4 ‘(1 2))
+;;;; (1 2 4)
+
+(defun list-insert-end (num lst)
+	(if (null lst)
+		(cons num nil)
+		(cons (first lst) (list-insert-end num (rest lst)))
+	)
+)
