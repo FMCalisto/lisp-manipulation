80bd815463cca83786337bc9adbc63d0419e8167
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-10-28 17:12:15 +0000

    Update list-insert-end.lisp

--- list/list-insert-end.lisp
+++ list/list-insert-end.lisp
@@ -1,13 +1,11 @@
 ;;;; Recursively the function inserts end receiving a x element into a list
 ;;;; and inserts the element at the end of the list.
-;;;; > (list-insert-end 5 ‘(1 2 4 6))
+;;;; > (list-insert-end 5 '(1 2 4 6))
 ;;;; (1 2 4 6 5)
-;;;; > (list-insert-end 4 ‘(1 2))
+;;;; > (list-insert-end 4 '(1 2))
 ;;;; (1 2 4)
 
 (defun list-insert-end (num lst)
 	(if (null lst)
 		(cons num nil)
-		(cons (first lst) (list-insert-end num (rest lst)))
-	)
-)
+		(cons (first lst) (list-insert-end num (rest lst)))))
