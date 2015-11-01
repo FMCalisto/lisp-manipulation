013338f7a7ffadd80131f29d03cbc631572b9303
Francisco Maria Calisto <FMCalisto@users.noreply.github.com>
2015-11-01 17:52:06 +0000

    Update sum.lisp

--- list/sum.lisp
+++ list/sum.lisp
@@ -1,6 +1,6 @@
 (defun sum (&rest l)
 	(labels ((interactive (res l)
-						(if (null l)
-							res
-							(iteractive (+ (first l) res) (rest l)))))
-		(iteractive 0 l)))
+		(if (null l)
+			res
+			(iteractive (+ (first l) res) (rest l)))))
+			(iteractive 0 l)))
